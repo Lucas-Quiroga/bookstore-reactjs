1cb5d65086332dbec7557a9e9d71c267900d6ade
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2022-11-03 17:07:18 -0300

    armando form

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -9,10 +9,125 @@ function Create() {
   const [completed, setCompleted] = useState(false);
   const [review, setReview] = useState("");
 
+  function handleChange(e) {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    switch (name) {
+      case "title":
+        setTitle(value);
+        break;
+
+      case "author":
+        setAuthor(value);
+        break;
+
+      case "intro":
+        setIntro(value);
+        break;
+
+      case "completed":
+        setCompleted(e.target.checked);
+        break;
+
+      case "review":
+        setReview(value);
+        break;
+      default:
+    }
+  }
+
+  //funcion que decofica una imagen local
+  const handleOnChangeFile = (e) => {
+    const element = e.target;
+    const file = element.files[0];
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file);
+
+    reader.onloadend = function () {
+      setCover(reader.result.toString());
+    };
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newBook = {
+      id: crypto.randomUUID(),
+      title,
+      author,
+      cover,
+      intro,
+      completed,
+      review,
+    };
+
+    //TODO: mandar a registrar el libro
+  };
+
   return (
     <div>
-      <form action="">
-        <div></div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <div>Title</div>
+          <input
+            type="text"
+            name="title"
+            onChange={handleChange}
+            value={title}
+          />
+        </div>
+
+        <div>
+          <div>Author</div>
+          <input
+            type="text"
+            name="author"
+            onChange={handleChange}
+            value={author}
+          />
+        </div>
+
+        <div>
+          <div>Cover</div>
+          <input type="file" name="cover" onChange={handleOnChangeFile} />
+          <div>
+            {!!cover ? <img src={cover} width="200" alt="preview" /> : ""}
+          </div>
+        </div>
+
+        <div>
+          <div>Introduccion</div>
+          <input
+            type="text"
+            name="intro"
+            onChange={handleChange}
+            value={intro}
+          />
+        </div>
+
+        <div>
+          <div>Completed</div>
+          <input
+            type="checkbox"
+            name="completed"
+            onChange={handleChange}
+            value={completed}
+          />
+        </div>
+
+        <div>
+          <div>Review</div>
+          <input
+            type="text"
+            name="review"
+            onChange={handleChange}
+            value={review}
+          />
+        </div>
+
+        <input type="submit" value="Register book" />
       </form>
     </div>
   );
